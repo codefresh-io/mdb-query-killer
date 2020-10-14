9e488ab1161c142a1900694e12b49d7f414588c4
alex-codefresh <a.cheshko@codefresh.io>
2020-10-14 17:30:35 +0300

    fix config killFilter env var parsing

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
+_ = require("lodash");
+
 const config = {
     mongoURI: process.env.MONGO_URI || 'mongodb://root:password@localhost:27017/admin?retryWrites=true&w=majority',
     thresholdSeconds: Number(process.env.THRESHOLD_SEC) || 60,
     checkIntervalSeconds: Number(process.env.CHECK_INTERVAL_SEC) || 15,
-    killFilter: JSON.parse(process.env.KILL_FILTER) || { op: 'query' },
+    killFilter: JSON.parse(_.get(process, 'env.KILL_FILTER', "{ \"op\": \"query\" }")),
     recordAllLongOps: Boolean(process.env.RECORD_ALL_LONG_OPS) || true,
     longOpsDB: process.env.LONG_OPS_DB || 'long-queries',
     longOpsCollection: process.env.LONG_OPS_COLLECTION || 'long-queries',
