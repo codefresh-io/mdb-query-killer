74041ba0c1304c41f148755b668efd7f34d4182b
alex-codefresh <a.cheshko@codefresh.io>
2020-10-15 16:11:49 +0300

    update default config values

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ _ = require("lodash");
 
 const config = {
     mongoURI: process.env.MONGO_URI || 'mongodb://root:password@localhost:27017/admin?retryWrites=true&w=majority',
-    thresholdSeconds: Number(process.env.THRESHOLD_SEC) || 60,
-    checkIntervalSeconds: Number(process.env.CHECK_INTERVAL_SEC) || 15,
+    thresholdSeconds: Number(process.env.THRESHOLD_SEC) || 90,
+    checkIntervalSeconds: Number(process.env.CHECK_INTERVAL_SEC) || 30,
     killFilter: JSON.parse(_.get(process, 'env.KILL_FILTER', "{ \"op\": \"query\" }")),
     recordAllLongOps: Boolean(process.env.RECORD_ALL_LONG_OPS) || true,
     longOpsDB: process.env.LONG_OPS_DB || 'long-queries',
